7c926b4a26f54899020941c8f40d651edd144187
jagm-create <jgrajalesmontes@gmail.com>
2026-09-15 10:03:04 -0500

    Redireccion

--- next.config.js
+++ next.config.js
@@ -1,4 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+ /** @type {import('next').NextConfig} */ const nextConfig = { async redirects() { return [ { source: '/', destination: '/ver', permanent: false, }, ]; }, }; module.exports = nextConfig;
